a1ace14374cb5b1eccb1c856d52fec7be67631a8
Galina <galina@Galinas-Air.lan>
2023-05-28 16:38:55 +0300

    Initial commit

---
+++ src/test/java/com/example/wapp/WappApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.wapp;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class WappApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
